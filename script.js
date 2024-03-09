6fff6a4d89fc5bf215684b4ef09c8b9770246c31
woongsnote <woongsnote@gmail.com>
2024-03-09 23:26:08 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// 페이지 로드 시 이전 상태 복원
+document.addEventListener("DOMContentLoaded", (event) => {
+  const buckets = document.querySelectorAll(".bucket");
+  buckets.forEach((bucket, index) => {
+    // 로컬 스토리지에서 상태 읽기
+    const isDone = localStorage.getItem("bucket" + index) === "done";
+    if (isDone) {
+      bucket.classList.add("done");
+    }
+  });
+});
+
+const buckets = document.querySelectorAll(".bucket");
+// 버킷 리스트 클릭 이벤트
+buckets.forEach((bucket, index) => {
+  bucket.addEventListener("click", function () {
+    // 클래스 토글
+    bucket.classList.toggle("done");
+
+    // 로컬 스토리지에 상태 저장
+    if (bucket.classList.contains("done")) {
+      localStorage.setItem("bucket" + index, "done");
+    } else {
+      localStorage.removeItem("bucket" + index);
+    }
+  });
+});
